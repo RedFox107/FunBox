dd0dfe9b2762084fade33e9b4b7a670c2f584b54
RedFox107 <e.ratnikov31@gmail.com>
2020-08-31 17:42:24 +0300

    Updates

---
+++ precache-manifest.e4071837ae084dcd328a92b508695523.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a1414004fb19fe62d5475316de0cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19e65ba4a0e3f09459c6",
+    "url": "/static/css/main.9c55c42f.chunk.css"
+  },
+  {
+    "revision": "85fc41a316eae60bff20",
+    "url": "/static/js/2.5bc61152.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5bc61152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e65ba4a0e3f09459c6",
+    "url": "/static/js/main.cfa0ea5e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "843a22b66c3047efdbad2e0c32385606",
+    "url": "/static/media/Cat.843a22b6.png"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "da7154cc0ce9cb91c3537f97953d132f",
+    "url": "/static/media/Pattern.da7154cc.png"
+  }
+]);
